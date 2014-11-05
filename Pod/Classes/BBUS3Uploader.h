46341ebda5281320dd22a2f5426c9c9b38e3a464
Boris Bügling <boris@icculus.org>
2014-11-05 10:51:11 +0100

    Upload method with file extension param -> public

--- Pod/Classes/BBUS3Uploader.h
+++ Pod/Classes/BBUS3Uploader.h
@@ -15,5 +15,9 @@
 @property (nonatomic, copy) NSString* path;
 
 -(instancetype)initWithBucket:(NSString*)bucket key:(NSString*)key secret:(NSString*)secret;
+-(void)uploadFileWithData:(NSData *)data
+            fileExtension:(NSString*)fileExtension
+        completionHandler:(BBUFileUploadHandler)handler
+          progressHandler:(BBUProgressHandler)progressHandler;
 
 @end
